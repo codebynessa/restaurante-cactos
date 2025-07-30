5713a4a69827d19b48c850195afcbd50a7e62053
vanessitta <vanessafreitasferreir4@gmail.com>
2025-07-30 00:08:55 -0400

    add arq

---
+++ js/dados.js
@@ -0,0 +1,519 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+              "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+                 "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+                        "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+                      "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+                    "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+                    "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+                    "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+                 "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+                     "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+                  "img": "./img/cardapio",
+            "name": "Carne",
+            "dsc": "Carne de Costela",
+            "price": 29.99
+        }
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+              "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+                  "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+               "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+                "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+                "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+                  "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+                 "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+                  "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+                  "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+                  "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+                 "img": "images/burguer.png",
+            "name": "XBurger",
+            "dsc": "XBurguer",
+            "price": 20
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+                 "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+                "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+               "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+               "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+                 "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+              "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+              "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "dessert-bar-care-package",
+                "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "donut-cookies-12-pack",
+                "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+              "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+               "img": "./img/",
+            "name": "Brownie®",
+            "dsc": "Brownie®",
+            "price": 15.99
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+                "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+              "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+              "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "margarita-mix",
+               "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+             "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+              "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+             "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+           "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+             "img": "./img",
+            "name": "Coca-Cola",
+            "dsc": "Coca-Cola",
+            "price": 10
+        }
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        }
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "bone-in-rib-steak",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+ "img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+   "img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+"img": "images/pizzas.png", 
+            "name": "Pizza",
+            "dsc": "Pizza",
+            "price": 50
+        }
+    ]
+}
